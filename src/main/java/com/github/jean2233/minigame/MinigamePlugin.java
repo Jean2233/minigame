89f99717794d48ac77c1dbe9d9633e93ebf44123
Jean2233 <jeanbassan2018@hotmail.com>
2021-11-24 13:49:29 -0300

    initial commit 🥂

---
+++ src/main/java/com/github/jean2233/minigame/MinigamePlugin.java
@@ -0,0 +1,17 @@
+package com.github.jean2233.minigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinigamePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
